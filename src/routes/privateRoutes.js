5362007d49ed70a62e55422707582460493721ea
bryandms <bryandms95@gmail.com>
2019-02-21 06:03:57 -0600

    Initial commit

--- src/routes/privateRoutes.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Redirect, Route } from "react-router-dom";
-import isAuth from "../utils/isAuth";
-
-const PrivateRoutes = props =>
-  isAuth() ? <Route {...props} /> : <Redirect to="/login" />;
-
-export default PrivateRoutes;
--- src/utils/verifyRole.js
+++
@@ -1,7 +0,0 @@
-export default (roles, role) => {
-  for (let i = 0; i < roles.length; i++) {
-    if (roles[i].name === role) return true;
-  }
-
-  return false;
-};
