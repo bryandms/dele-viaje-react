811b834827d3472b90d4cec8e2a6757767a7c949
bryandms <bryandms95@gmail.com>
2019-02-27 14:56:52 -0600

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import { Provider } from 'react-redux'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Routes from './routes/index'
+import store from './store/index'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import 'semantic-ui-css/semantic.min.css'
+import './index.css'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  request: async (operation) => {
+    const token = await localStorage.getItem('token') || ''
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  }
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  </ApolloProvider>,
+  document.getElementById('root')
+)
+
+serviceWorker.unregister()
